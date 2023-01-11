715cdc5d57fdfcc717b50ead0e740e68813e4c4e
thesanjaysubedi <105510768+thesanjaysubedi@users.noreply.github.com>
2023-01-11 17:19:29 +0545

    Create command.js

---
+++ command.js
@@ -0,0 +1,10 @@
+const { exec } = require('child_process');
+
+exec('ls -l /home', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
